cc36c46c17bc1fb8f902c6840e5fa3045a04d62a
sickodev <kbishwa03kalyan@gmail.com>
2024-07-25 18:34:37 +0530

    added controls and customisers

--- src/main.ts
+++ src/main.ts
@@ -5,25 +5,29 @@ import { stars } from "./stars";
 import { sunMesh } from "./sun";
 import { ambientLight, sunlight } from "./lights";
 import Planet from "./planet";
+import { gui } from "./gui";
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById("root")?.appendChild(renderer.domElement);
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
 );
 const orbitControls = new OrbitControls(camera, renderer.domElement);
 camera.position.set(0, 20, 100);
 
 orbitControls.update();
 
-//adding axes helper
-const axesHelper = new THREE.AxesHelper(200);
-scene.add(axesHelper);
+const controls = document.getElementById("controls");
+window.onload = () => {
+  controls?.addEventListener("click", () => {
+    alert("Uh uh uh");
+  });
+};
 
 //Creating A Solar System
 const solarSystem = new THREE.Group();
@@ -57,11 +61,11 @@ jupiterSystem.add(jupiterMesh);
 
 //Adding to Solar System
 solarSystem.add(
-    mercurySystem,
-    venusSystem,
-    earthSystem,
-    marsSystem,
-    jupiterSystem
+  mercurySystem,
+  venusSystem,
+  earthSystem,
+  marsSystem,
+  jupiterSystem
 );
 
 // Adding to scene
@@ -70,29 +74,38 @@ scene.add(solarSystem);
 scene.add(ambientLight);
 scene.add(sunlight);
 
+//Adding UI ELEMENTS
+const options = {
+  LightColor: 0xaaaaaa,
+};
+gui.addColor(options, "LightColor").onChange((e) => {
+  ambientLight.color.set(e);
+});
+
+//constants
 const EARTH_YEAR = 2 * Math.PI * (1 / 300) * (1 / 60);
 const EARTH_DAY = 2 * Math.PI * (1 / 90) * (1 / 60);
 
 function animate() {
-    sunMesh.rotation.y += 0.001;
+  sunMesh.rotation.y += 0.001;
 
-    mercuryMesh.rotation.y += 58.6 * EARTH_DAY;
-    mercurySystem.rotation.y += (1 / 0.24) * EARTH_YEAR;
+  mercuryMesh.rotation.y += 58.6 * EARTH_DAY;
+  mercurySystem.rotation.y += (1 / 0.24) * EARTH_YEAR;
 
-    venusMesh.rotation.y += -243 * EARTH_DAY;
-    venusSystem.rotation.y += (1 / 0.62) * EARTH_YEAR;
+  venusMesh.rotation.y += -243 * EARTH_DAY;
+  venusSystem.rotation.y += (1 / 0.62) * EARTH_YEAR;
 
-    earthMesh.rotation.y += EARTH_DAY;
-    earthSystem.rotation.y += EARTH_YEAR;
+  earthMesh.rotation.y += EARTH_DAY;
+  earthSystem.rotation.y += EARTH_YEAR;
 
-    marsMesh.rotation.y += 1.001 * EARTH_DAY;
-    marsSystem.rotation.y += (1 / 1.88) * EARTH_YEAR;
+  marsMesh.rotation.y += 1.001 * EARTH_DAY;
+  marsSystem.rotation.y += (1 / 1.88) * EARTH_YEAR;
 
-    jupiterMesh.rotation.y += 0.41 * EARTH_DAY;
-    jupiterSystem.rotation.y += (1 / 11.86) * EARTH_YEAR;
+  jupiterMesh.rotation.y += 0.41 * EARTH_DAY;
+  jupiterSystem.rotation.y += (1 / 11.86) * EARTH_YEAR;
 
-    //main rendering
-    renderer.render(scene, camera);
+  //main rendering
+  renderer.render(scene, camera);
 }
 
 renderer.setAnimationLoop(animate);
--- src/planet.ts
+++ src/planet.ts
@@ -16,7 +16,7 @@ export default class Planet {
     if (this.mesh === undefined || this.mesh === null) {
       const geometry = new THREE.SphereGeometry(this.radius);
       const texture = new THREE.TextureLoader().load(`/${this.texture}`);
-      const material = new THREE.MeshBasicMaterial({ map: texture });
+      const material = new THREE.MeshStandardMaterial({ map: texture });
       this.mesh = new THREE.Mesh(geometry, material);
       this.mesh.position.x += this.positionX;
     }
