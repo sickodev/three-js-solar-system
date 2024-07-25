1b633b4392bc0f07c5a3a97324f5d41b7f3edc08
sickodev <kbishwa03kalyan@gmail.com>
2024-07-25 17:47:28 +0530

    texture fix

--- src/planet.ts
+++ src/planet.ts
@@ -16,7 +16,7 @@ export default class Planet {
         if (this.mesh === undefined || this.mesh === null) {
             const geometry = new THREE.SphereGeometry(this.radius);
             const texture = new THREE.TextureLoader().load(
-                `../public/textures/${this.texture}`
+                `../public/${this.texture}`
             );
             const material = new THREE.MeshBasicMaterial({ map: texture });
             this.mesh = new THREE.Mesh(geometry, material);
